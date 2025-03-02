2ca8ce98f62f862cfc6b693b529fff81a6b8ae10
Gawc1uuu <gawlikjakub9@gmail.com>
2025-03-02 22:21:51 +0100

    transactions table

--- be/src/db/schema.ts
+++ be/src/db/schema.ts
@@ -1,8 +1,32 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { numeric, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { ulid } from 'ulid'
+import { pgEnum } from "drizzle-orm/pg-core";
+
+export const transactionTypeEnum = pgEnum("transaction_type", [
+    "deposit",
+    "credit",
+]);
+
+export const transactionSubTypeEnum = pgEnum("transaction_sub_type", [
+    "reward",
+    "purchase",
+    "refund",
+]);
+
+export const transactionStatusEnum = pgEnum("transaction_status", [
+    "pending",
+    "completed",
+    "failed",
+]);
+
 
 export const transactionsTable = pgTable("transactions", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    age: integer().notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
+    transactionId: varchar('transaction_id').primaryKey().$defaultFn(ulid),
+    userId: varchar('user_id').notNull(),
+    type: transactionTypeEnum('type').notNull(),
+    subType: transactionSubTypeEnum('sub_type').notNull(),
+    amount: numeric("amount", { precision: 10, scale: 2 }).notNull(),
+    status: transactionStatusEnum('status').notNull(),
+    description: text('description'),
+    createdAt: timestamp('created_at').defaultNow()
 });
